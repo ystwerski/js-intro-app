b85d641d134493f5a93a115757948d2296b7e363
Jay Wengrow <jaywngrw@gmail.com>
2014-08-03 01:00:48 -0500

    initial exercises

---
+++ app/assets/javascripts/exercises.js
@@ -0,0 +1,29 @@
+// // Exercise #1:
+
+// Do the NASA countdown through the console. That is, the console should say:
+
+// 10
+// 9
+// 8
+// 7
+// 6
+// 5
+// 4
+// 3
+// 2
+// 1
+// BLASTOFF!
+
+// Hint: Use a for loop!
+
+// Exercise #2:
+
+var numbers = [5, 23, 98, 234, 1, 0, 3, 177];
+
+// Write some code that prints all the numbers in the 'numbers' array to the console.
+// Hint: Use a for loop!
+
+// Exercise #3:
+
+// Write some code that finds the highest number in the 'numbers' array.
+// Hint: Use a for loop!
--- app/assets/javascripts/reference.js
+++ app/assets/javascripts/reference.js
@@ -120,6 +120,37 @@
 //   console.log(i);
 // }
 
+// // Random numbers:
+
+// // random number between 1 and 10:
+
+// Math.floor((Math.random() * 10) + 1);
+
+// // Constructors (the closest thing to Ruby classes)
+
+// function Car( model, year, miles ) {
+ 
+//   this.model = model;
+//   this.year = year;
+//   this.miles = miles;
+ 
+//   this.toString = function () {
+//     return this.model + " has done " + this.miles + " miles";
+//   };
+// }
+ 
+// // Usage:
+ 
+// // We can create new instances of the car
+// var civic = new Car( "Honda Civic", 2009, 20000 );
+// var mondeo = new Car( "Ford Mondeo", 2010, 5000 );
+ 
+// // and then open our browser console to view the
+// // output of the toString() method being called on
+// // these objects
+// console.log( civic );
+// console.log( mondeo.toString() );
+
 
 
 
