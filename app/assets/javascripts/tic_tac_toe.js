5cb87ee7353c3e78939eed34565c29afe8e5207a
Jay Wengrow <jaywngrw@gmail.com>
2014-08-03 02:04:33 -0500

    beginning of tic-tac-toe app

--- app/assets/javascripts/tic_tac_toe.js
+++ app/assets/javascripts/tic_tac_toe.js
@@ -1,13 +1,31 @@
 var spaces = ["", "", "", "", "", "", "", "", ""];
 var currentTurn = "X";
 
+function assignSpace(spaceId) {
+  spaces[spaceId] = currentTurn;
+}
+
+function updateDisplay() {
+
+}
+
+function checkIfWon() {
+
+}
+
+function switchTurns() {
+
+}
+
 $(function() {
 
   $(".block").click(function() {
     var spaceId = parseInt($(this).attr("id"));
+    assignSpace(spaceId);
     console.log(spaces);
-
-
+    updateDisplay();
+    checkIfWon();
+    switchTurns();
   });
 
 });
