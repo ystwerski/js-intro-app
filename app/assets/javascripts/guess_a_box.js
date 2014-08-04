7c4fd8d3631bd84638a0aa794acf337627c2e446
Jay Wengrow <jaywngrw@gmail.com>
2014-08-04 12:28:02 -0500

    better guess a box game

--- app/assets/javascripts/guess_a_box.js
+++ app/assets/javascripts/guess_a_box.js
@@ -1,36 +1,30 @@
-var rightAnswer = Math.floor((Math.random() * 3));
+// Picks a random number between 0 and 2:
+var winningBox = Math.floor((Math.random() * 3));
 
-function guessNumber(numberGuessed) {
+// function guessBox(boxGuessed) {
 
-  if (numberGuessed === rightAnswer) {
-    alert("You win!");
-  } else {
-    console.log("wrong");
-  }
-
-}
-
-// // More advanced version:
-
-// var rightAnswer = Math.floor((Math.random() * 3));
-
-// function guessNumber(numberGuessed, box) {
-
-//   if (numberGuessed === rightAnswer) {
-//     box.className = box.className + " right";
+//   if (boxGuessed === winningBox) {
 //     alert("You win!");
 //   } else {
-//     box.className = box.className + " wrong";
 //     console.log("wrong");
 //   }
 
 // }
 
+// More advanced version:
 
+var winningBox = Math.floor((Math.random() * 3));
 
+function guessBox(boxGuessed, box) {
 
+  if (boxGuessed === winningBox) {
+    box.className = box.className + " right";
+    document.getElementById("winning-message").innerHTML = "You win!";
+  } else {
+    box.className = box.className + " wrong";
+  }
 
-
+}
 
 
 
