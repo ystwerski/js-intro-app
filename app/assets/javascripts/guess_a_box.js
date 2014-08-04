4ccff85f1e894cded3c78b86c43207c1ea431269
Jay Wengrow <jaywngrw@gmail.com>
2014-08-04 11:38:00 -0500

    remove jquery from guessing game

--- app/assets/javascripts/guess_a_box.js
+++ app/assets/javascripts/guess_a_box.js
@@ -1,20 +1,38 @@
 var rightAnswer = Math.floor((Math.random() * 3));
 
-$(function() {
+function guessNumber(numberGuessed, box) {
 
-  $("#box-game .block").click(function() {
-    var numberGuessed = parseInt($(this).attr("id"));
+  if (numberGuessed === rightAnswer) {
+    alert("You win!");
+  } else {
+    console.log("wrong");
+  }
 
-    if (numberGuessed === rightAnswer) {
-      $(this).addClass("right");
-      alert("You win!");
-    } else {
-      $(this).addClass("wrong");
-    }
+}
 
-  })
+// // More advanced version:
 
-});
+// var rightAnswer = Math.floor((Math.random() * 3));
+
+// function guessNumber(numberGuessed, box) {
+
+//   if (numberGuessed === rightAnswer) {
+//     box.className = box.className + " right";
+//     alert("You win!");
+//   } else {
+//     box.className = box.className + " wrong";
+//     console.log("wrong");
+//   }
+
+// }
+
+
+
+
+
+
+
+
 
 
 
