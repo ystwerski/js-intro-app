1e0dbf71bd3dbf757e91c891fe46904cdb741a29
Jay Wengrow <jaywngrw@gmail.com>
2014-08-06 13:40:28 -0500

    basic abc solution

--- app/assets/javascripts/abc.js
+++ app/assets/javascripts/abc.js
@@ -1,5 +1,13 @@
 var abcs = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var placeHolder = 0;
 
 function clickLetter(letterBox) {
-  
+  var letter = letterBox.innerHTML;
+  if(letter === abcs[placeHolder]) {
+    placeHolder++;
+    letterBox.style.visibility = "hidden";
+  }
+  if(placeHolder == 26) {
+    alert("You win!");
+  }
 }
