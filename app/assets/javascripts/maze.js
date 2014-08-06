3c173d0196096d6865e959a089a7e888c822e215
Jay Wengrow <jaywngrw@gmail.com>
2014-08-06 15:21:22 -0500

    basic maze solution

--- app/assets/javascripts/maze.js
+++ app/assets/javascripts/maze.js
@@ -1,5 +1,16 @@
-
+var path = [1, 2, 3, 4, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 6, 7, 8, 0, 0, 0, 0, 0, 0, 9, 10];
+var whereAt = 1;
 
 function moveToSegment(space, box) {
-
+  if (path[space] === whereAt) {
+    box.style["background-color"] = "yellow";
+    whereAt += 1;
+    if (whereAt === 11) { alert("You win!"); }
+  } else {
+    boxes = document.getElementsByClassName("box");
+    for(var i = 0; i < boxes.length; i++) {
+      boxes[i].style["background-color"] = "blue";
+    }
+    whereAt = 1;
+  }
 }
