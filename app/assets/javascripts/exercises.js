ec14628c8c9e627057107f3df094cd4119b8e2da
Jay Wengrow <jaywngrw@gmail.com>
2014-11-23 14:08:10 -0600

    added fizzbuzz instructions

--- app/assets/javascripts/exercises.js
+++ app/assets/javascripts/exercises.js
@@ -31,7 +31,9 @@
 
 // // Exercise #4:
 
-// Implement FizzBuzz in JavaScript.
+// Implement FizzBuzz in JavaScript. Write a program that prints the integers from 1 to 100.
+// But for multiples of three print "Fizz" instead of the number, and for the multiples of five print "Buzz". 
+// For numbers which are multiples of both three and five print "FizzBuzz".
 
 // // Exercise #5:
 
