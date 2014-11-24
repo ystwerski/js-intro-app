3017be9f728c204c5ebddc2dccee05706e870271
Jay Wengrow <jaywngrw@gmail.com>
2014-11-24 16:38:50 -0600

    better variable name

--- app/assets/javascripts/guess_a_box.js
+++ app/assets/javascripts/guess_a_box.js
@@ -1,8 +1,8 @@
 var winningBox = Math.floor((Math.random() * 3)); // Picks a random number between 0 and 2:
 
-function guessBox(boxGuessed) {
+function guessBox(boxNumber) {
 
-  if (boxGuessed === winningBox) {
+  if (boxNumber === winningBox) {
     alert("You win!");
   } else {
     console.log("wrong");
